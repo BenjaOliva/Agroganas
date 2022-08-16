0325a97b5d78f45ca36f817eae73ebc3ed2503aa
Benjamin Oliva <benjaminoliva14@gmail.com>
2022-08-15 22:44:59 -0300

    Constants file ✍
    Adding the constant file for future stuff

---
+++ src/utils/constants.ts
@@ -0,0 +1,24 @@
+export const categoriesConverter = [
+  {
+    value: '',
+    label: 'All',
+  },
+];
+
+export const currenciesConverter = [
+  {
+    value: 'Pesos Argentinos',
+    symbol: '$',
+    short: 'ARS',
+  },
+  {
+    value: 'Dólares',
+    symbol: 'U$D',
+    short: 'USD',
+  },
+  {
+    value: 'Euros',
+    symbol: '€',
+    short: 'EUR',
+  },
+];
